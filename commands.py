1bbc15a23e0018503f9f298d96c1cc3c6e4cfe7d
D Anzorge <d.anzorge@gmail.com>
2011-06-30 18:37:49 -0400

    Made unicode support for translate gooder

--- commands.py
+++ commands.py
@@ -1,4 +1,4 @@
-__author__ = "pyton_guy"
+﻿__author__ = "pyton_guy"
 
 """
 Commands for the #/r/nyc Freenode IRC bot
@@ -183,15 +183,18 @@ def translate(self, user, channel, args):
 
     """	Translates text into english from a detected language. """
     if args:
-        data = json.load(urllib2.urlopen(
-            "https://www.googleapis.com/language/translate/v2?key=AIzaSyD_Qg6EVKkhRnUAYBwG5UL4sBjzJTJgX7k&q=%s&target=en" % (
-            "%20".join(args.split(" ")))))
-        text = str(data['data']['translations'][0]['translatedText'])
+        translate_url = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD_Qg6EVKkhRnUAYBwG5UL4sBjzJTJgX7k&q={0}&target=en" 
+        translate_url = translate_url.format(urllib2.quote(args))
+        data = json.load(urllib2.urlopen(translate_url))
+        # unicoding this because str() would freak out if given utf8 text
+        text = unicode(data['data']['translations'][0]['translatedText'])
         language = str(data['data']['translations'][0]['detectedSourceLanguage'])
-        if args == text:
-            reply = "No translation found."
+        # unicode to unicode comparison
+        if args.decode('utf8') == text:
+           reply = "No translation found."
         else:
-            reply = "\"%s\" Translated from: %s" % (text, language)
+           # encode text for sending 
+           reply = "\"%s\" Translated from: %s" % (text.encode('utf-8'), language)
         self.msg(channel, reply)
     else:
         self.msg(channel,

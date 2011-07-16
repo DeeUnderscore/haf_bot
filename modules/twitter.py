bf10fa565d0a5a8da2bc0edc0ed64af11ce53b79
D Anzorge <d.anzorge@gmail.com>
2011-07-15 22:02:10 -0400

    Added reply to twitter

--- modules/twitter.py
+++ modules/twitter.py
@@ -2,10 +2,13 @@
 Twitter module for twiting twits on twitter
 """
 
+# yes, I know they're called "tweets"
+
 import oauth2 as oauth
 import time
-import urllib
+import urllib, urllib2
 import json
+import re
 from lib.botconfig import config
 from BotModule import BotModule
 
@@ -45,6 +48,68 @@ def twit(self, user, channel, args):
         self.msg(channel, "Successfully twitted: {0} ({1})".format(jcontent["text"].encode("utf-8"), status_url))
         # TODO: Use bitly?
         
+def reply_twit(self, user, channel, args):
+    """
+    Replies to a given twit by id
+	
+	The needs to be provided a tweet id to respond to. It will parse the id
+	out of a URL if it is given one.
+	""" 
+    nick = user.split("!", 1)[0]
+    
+    if not args:
+        self.msg(channel, "{0}: Needs an id and a message.".format(nick))
+        return
+    
+    args = args.split(" ", 1)
+    tid = args[0]
+    tweet = args[1]
+    
+    if len(tweet) > 140:
+        self.msg(channel, "{0}: Sorry, but that's more than one hundred and forty characters, and Twitter is pretty serious about not allowing that.".format(nick))
+        return 
+
+    # extract the id 
+    if not tid.isdigit():
+        # we need to include "status" because twitter names can be all digits
+        matched = re.search("status/(\d+)(?:/.*)?$", tid)
+        if matched:
+            tid = matched.group(1)
+        else:
+            self.msg(channel, "{0}: Could not find twit id to reply to.".format(nick))
+            return 
+            
+    # show requires GET so we don't need to send twenty different tokens here
+    url = base_url + "statuses/show/" + tid + ".json"
+    
+    try:
+        content = urllib2.urlopen(url)
+    except urllib2.HTTPError:
+        self.msg(channel, "{0}: Could not find the twit I'm supposed to be replying to. Check id.".format(nick))
+        return
+    
+    jcontent = json.load(content)
+    reply_to = '@' + jcontent['user']['screen_name']
+        
+    # Twitter will disregard the reply to field if the name of the addressee is 
+    # not mentioned in the message text itself, so we check for that
+    if not reply_to in tweet:
+        self.msg(channel, "{0}: Twitter requires that you include the name of the addressee ('{1}') in your message.".format(nick, reply_to))
+        return
+
+    # finally, we should be all ok to send this out
+    url = base_url + "statuses/update.json"
+    params = {'status': tweet, 'in_reply_to_status_id': tid}
+    
+    resp, content = twit_request(url, params)
+    
+    if resp["status"] != "200":
+        self.msg(channel, "{0}: Well, it looked like a valid twit, but Twitter refused to post it.".format(nick))
+    else:
+        jcontent = json.loads(content)
+        status_url = "https://twitter.com/{0}/status/{1}".format(account, jcontent["id_str"])
+        self.msg(channel, "Reply posted: {0} ({1})".format(jcontent["text"].encode("utf-8"), status_url))
+
 def twit_request(url, args):
     """Handler for twitter requests"""
     
@@ -64,5 +129,5 @@ def twit_request(url, args):
     return (resp, content)
     
     
-bot_modules = [BotModule({"twit": twit})]
+bot_modules = [BotModule({"twit": twit, "twit.reply": reply_twit})]
 print "Twit module loaded."

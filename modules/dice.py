a948fd0bcee898018393f50df8c7a784ef11daf8
D Anzorge <d.anzorge@gmail.com>
2011-07-20 16:31:16 -0400

    Added dice module

---
+++ modules/dice.py
@@ -0,0 +1,32 @@
+import re
+import random
+from BotModule import BotModule
+
+def roll_dice(self, user, channel, args):
+    
+    if args.isdigit():
+        dice = 1
+        sides = int(args)
+    else:
+        match = re.search("(\d+)d(\d+)", args)
+        if match:
+            dice = int(match.group(1))
+            sides = int(match.group(2))
+        else:
+            dice = 1
+            sides = 6
+    
+    rolls = []
+    
+    for i in range(dice):
+        rolls.append(str(random.randint(1, sides)))
+    
+    user = user.split("!", 1)[0]
+    
+    result = "Rolled {0}d{1}: ".format(dice, sides)
+    result +=  (' '.join(rolls))
+    
+    
+    self.msg(channel, user + ": " + result )
+        
+dice = BotModule({"roll": roll_dice})

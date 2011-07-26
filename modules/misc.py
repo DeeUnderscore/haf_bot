60d51cb77deb1b8e70a5e0840faedc4dacb6d772
D Anzorge <d.anzorge@gmail.com>
2011-07-26 16:22:48 -0400

    Integrated rage.thewaffleshop.net

---
+++ modules/misc.py
@@ -0,0 +1,13 @@
+from BotModule import BotModule
+import urllib2
+
+
+def fuck(self, user, channel, args):
+    """
+    https://plus.google.com/111528911333263728657/posts/cSx5w5aaUv1
+    """
+    
+    fucks = urllib2.urlopen('http://rage.thewaffleshop.net/').read()
+    self.msg(channel, fucks)
+    
+misc = BotModule({'fuck': fuck})
